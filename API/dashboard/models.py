b64484d149452c6de922b07bcc9ad141ea1fa431
zxkeyy <z_selmani@estin.dz>
2023-12-15 03:08:09 +0100

    Models: added Guest and Booking modela

--- API/dashboard/models.py
+++ API/dashboard/models.py
@@ -7,6 +7,21 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     pass
 
+class Guest(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    date_of_birth = models.DateField()
+    address = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    nationality = models.CharField(max_length=255)
+
+class Payment(models.Model):
+    booking = models.ForeignKey('Booking', on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_date = models.DateTimeField()
+    payment_method = models.CharField(max_length=255)
+
 class RoomType(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
@@ -15,4 +30,13 @@ class RoomType(models.Model):
 
 class Room(models.Model):
     status = models.CharField(max_length=255)
-    type = models.ForeignKey(RoomType, on_delete=models.PROTECT)
+    type = models.ForeignKey(RoomType, on_delete=models.CASCADE)
+
+class Booking(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    guest = models.ForeignKey(Guest, on_delete=models.CASCADE)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    check_in = models.DateTimeField()
+    check_out = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
